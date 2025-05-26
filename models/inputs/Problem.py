d7925720c134b54d259f96d1bfd3e6a27fe7d445
Cristina Tobar <cristina.tobar@oga.ai>
2025-05-26 08:50:12 +0200

    Extendemos los datos

--- models/inputs/Problem.py
+++ models/inputs/Problem.py
@@ -21,8 +21,8 @@ class Problem:
 
         self.problem_name = problem_name
 
-        self.cost_per_km = 100
-        self.cost_per_no_del_demand = 10
+        self.cost_per_km = 1
+        self.cost_per_no_del_demand = 0.1
         self.truck_capacity_per_customer = 100
 
         self.customers_map = {}
@@ -36,6 +36,9 @@ class Problem:
         self.node_data = None
         self.demand_data = None
 
+        self.initial_validation_cost = {}
+        self.all_real_demands = None
+
     @handle_exceptions
     def feed(self):
         self.read_data()
@@ -46,7 +49,7 @@ class Problem:
             day_data = real_demand[real_demand['codnode'] == row['codnode']]
             node_data = node_data_model[node_data_model['codnode'] == row['codnode']]
             val_size = 0.2
-            k = 80 # int(len(record_data) * val_size) TODO
+            k = 100 # int(len(record_data) * val_size) TODO
             # Prepare data for KNN
             X_train = record_data.drop(columns=['Pallets', 'codnode', 'Date', 'Year', 'Holiday'])  # Drop non-feature columns
             y_train = record_data['Pallets']  
@@ -94,10 +97,48 @@ class Problem:
         node_data = pd.read_csv(node_data_github_link, sep=';', encoding='latin-1') 
 
         demand_data = pd.read_csv(demand_data_github_link, sep=';', encoding='latin-1') 
-        demand_data['Date'] = pd.to_datetime(demand_data['Date'], format="%d/%m/%Y")      
+        demand_data['Date'] = pd.to_datetime(demand_data['Date'], format="%d/%m/%Y")     
+
+        demand_data = self.extend_demand_data(demand_data)
 
         self.node_data = node_data
         self.demand_data = demand_data
+
+    @handle_exceptions
+    def extend_demand_data(self, demand_data):
+        print(demand_data.columns)
+        print(demand_data.head())
+        # Define the number of new records to create per existing record
+        n = 5  # Por ejemplo, crear 5 registros adicionales por cada registro existente
+        
+        # Create new records with noise stratified by codnode
+        selected_columns = ['Pallets', 'WIND SPEED (m/s)', 'WIND DIRECTION (Â°)', 
+                    'TEMPERATURE (ÂºC)', 'RELATIVE HUMIDITY (%)', 
+                    'BARIOMETRIC PRESSURE (mb)', 'SOLAR RADIATION (W/m2)', 
+                    'PRECIPITATION (l/m2)']
+        new_records = []
+        for _, group in demand_data.groupby('codnode'):
+            # Calculate the standard deviation for each column within the group
+            noise_std = group[selected_columns].std()
+            
+            for _, row in group.iterrows():
+                for _ in range(n):
+                    new_record = row.copy()
+                    for column in noise_std.index:
+                        if column in ['Pallets', 'WIND SPEED (m/s)', 'WIND DIRECTION (Â°)', 
+                                      'TEMPERATURE (ÂºC)', 'RELATIVE HUMIDITY (%)', 
+                                      'BARIOMETRIC PRESSURE (mb)', 'SOLAR RADIATION (W/m2)', 
+                                      'PRECIPITATION (l/m2)']:
+                            # Add noise and ensure the value is positive
+                            new_value = row[column] + random.gauss(0, noise_std[column])
+                            if new_value < 0:
+                                new_value = row[column]
+                    new_records.append(new_record)
+        
+        # Append new records to the original data
+        extended_demand_data = pd.concat([demand_data, pd.DataFrame(new_records)], ignore_index=True)
+        
+        return extended_demand_data
 
     @handle_exceptions
     def add_customer(self, codnode, record_data, record_data_train, record_data_val, day_data, node_data):
@@ -129,7 +170,11 @@ class Problem:
         return geodesic((customer1.latitude, customer1.longitude), (customer2.latitude, customer2.longitude)).kilometers
     
     def get_all_real_demands(self):
-        return sum([customer.real_demand for customer in self.customers_list])
+        if self.all_real_demands is None:
+            self.all_real_demands = sum([customer.real_demand for customer in self.customers_list])
+        return self.all_real_demands
 
-
-
+    def get_initial_validation_cost(self, scenario):
+        if scenario not in self.initial_validation_cost:
+            self.initial_validation_cost[scenario] = sum([customer.demand_validation_scenarios[scenario] for customer in self.customers_list]) * self.cost_per_no_del_demand
+        return self.initial_validation_cost[scenario]
